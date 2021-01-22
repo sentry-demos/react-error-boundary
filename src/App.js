2e7c012b311bf0039c657ffb09988196f5fbeb91
William Capozzoli <thinkocapo@gmail.com>
2021-01-22 14:30:20 -0500

    separated fallback component, and cleanup

--- src/App.js
+++ src/App.js
@@ -9,60 +9,55 @@ class App extends Component {
     this.state = {
       message: "This message is stored in a property on the component's state and it needs to be a string",
     };
-  }
-  /*
-  * Any errors thrown in a lifecycle callback will have a componentStack
-  * componentDidMount is a lifecycle callback and responsible for helping render the component tree
-  */
-  componentDidMount() {
-    // this.iWillMakeAComponentStack()
+    this.fallbackComponent = this.fallbackComponent.bind(this)
   }
 
-  /*
-  * Errors thrown from this method will not have a componentStack, because this method is not lifecycle callback.
-  * so you will not see componentStack on the Issue in Sentry
-  */
+  // Any errors thrown in a lifecycle callbacks will have a componentStack
+  componentDidMount() {
+    // this.iCrashTheComponentTree()
+  }
+
+  // Errors thrown from this method will not have a componentStack, because this method is not lifecycle callback.
   nonLifecycleMethod() {
-    // this.tryMe()
+    // this.iDontCrashTheComponentTree()
+  }
+
+  fallbackComponent({ error, componentStack, resetError }) { 
+    return (
+    <React.Fragment>
+        <h3>Fallback Component</h3>
+        <div>
+        React encountered an error, but the Fallback component specified by your Sentry ErrorBoundary is now displaying, 
+        rather than the crashed component tree (it was expecting a String, not an Object).
+        </div>
+        <div>
+        Use a styled fallback component and message here for your customer to see, rather than letting them
+        see what the React framework does when it crashes on itself!
+        </div>
+        
+        <h3>Error</h3>
+        <div>{error.toString()}</div>
+        
+        <h3>Component Stack:</h3>
+        <div>{componentStack}</div>
+        
+        <br></br>
+        
+        <button
+          onClick={() => {
+            this.setState({ message: "I am a string value, and will restore the component tree from its failed state!" });
+            resetError();
+          }}>
+          Click here to reset!
+        </button>
+      </React.Fragment>
+    )
   }
 
   render() {
-    const warningText = `
-    React encountered an error, but the Fallback component specified by your Sentry ErrorBoundary is now displaying, 
-    rather than the crashed component tree (it was expecting a String, not an Object).
-    `
-    const suggestion = `
-    Use a styled fallback component and message here for your customer to see, rather than letting them
-    see what the React framework does when it crashes on itself!
-    `
-
     return (
       // This 'fallback' component is what displays when the React component tree crashes on itself
-      <Sentry.ErrorBoundary
-              fallback={({ error, componentStack, resetError }) => (
-                <React.Fragment>
-                  <h3>Fallback Component</h3>
-                  <div>{warningText}</div>
-                  <div>{suggestion}</div>
-                  
-                  <h3>Error:</h3>
-                  <div>{error.toString()}</div>
-                  
-                  <h3>Component Stack:</h3>
-                  <div>{componentStack}</div>
-                  
-                  <br></br>
-                  
-                  <button
-                    onClick={() => {
-                      this.setState({ message: "I am a string value, and will restore the component tree from its failed state!" });
-                      resetError();
-                    }}>
-                    Click here to reset!
-                  </button>
-                </React.Fragment>
-              )}
-            >
+      <Sentry.ErrorBoundary fallback={this.fallbackComponent}>
               <h3>Component State:</h3>
               <div>{this.state.message}</div>
 
