f8bc50394d847142eecf3dcaf58e207981bdd77d
William Capozzoli <thinkocapo@gmail.com>
2021-01-22 14:47:24 -0500

    clean

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react";
 import * as Sentry from '@sentry/react';
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import * as Sentry from '@sentry/react';
 Sentry.init({
   dsn: "https://426a31c1b1614727b705cdece09484ff@o262702.ingest.sentry.io/5533979",
   release: process.env.REACT_APP_RELEASE,
-  environment: "dev",
-  debug: true
+  environment: "test",
+  // debug: true
 });
 
 ReactDOM.render(
@@ -19,8 +19,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
